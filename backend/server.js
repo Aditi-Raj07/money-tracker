45ce9d22c82a86d7b9c4e3730f4029a334ebce4a
Aditi Raj <aditiraj4176@gmail.com>
2025-07-05 14:30:41 +0530

    backendtwo

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const express=require("express");
 const cors=require("cors");
 const path=require("path");
 const connectDB=require("./config/db");
+const authRoutes = require("./routes/authRoutes")
+
+
 const app=express();
 
 //Middleware to handle cors
@@ -17,5 +20,7 @@ app.use(
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/v1/auth",authRoutes);
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>console.log(`Server runnning on port ${PORT}`));
